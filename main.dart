18015b4ae70cd1b2ba03683695e33ff5e7cf2618
waleedx7 <w.alshelahi@gmail.com>
2023-02-20 00:12:43 +0300

    done

--- main.dart
+++ main.dart
@@ -2,9 +2,22 @@ void main() {
   final movie = {
     'name': 'The Amazing SpiderMan',
     'genre': 'Action',
-    'ratings': [1.5,3.2,6.4]
+    'ratings': [10]
   };
   print(movie['name']);
   print(movie['genre']);
   print(movie['ratings']);
+}
+
+class Movie {
+  String name;
+  String genre;
+  List<double> ratings;
+  Movie(this.name, this.genre, this.ratings);
+
+  void printDetails() {
+    print("Name:$name");
+    print("Genre:$genre");
+    print("Ratings:$ratings");
+  }
 }
